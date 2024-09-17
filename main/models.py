ce13e75c53cc6a2e57f2c14900457c0e8dfc0ef3
freiazulaika <freiazulaika27@gmail.com>
2024-09-17 22:56:46 +0700

    Update Tugas 3

--- main/models.py
+++ main/models.py
@@ -1,10 +1,10 @@
 from django.db import models
+import uuid
 
-# Create your models here.
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
     stock = models.IntegerField()
-    note = models.CharField(max_length=255)
     size = models.IntegerField()
